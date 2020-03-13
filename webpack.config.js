f413411bdfc3659c5d4d3ecca4138a95623397ff
stoica94 <ovidiu.stoica1094@gmail.com>
2020-03-13 08:32:30 +0200

    Moved project to it's own folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/app.ts",
+  entry: "./drag-drop-project/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: "./src/app.ts",
+  entry: "./drag-drop-project/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
